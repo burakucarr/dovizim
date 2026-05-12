b5e5506dbc6a91444d97b3720875e63c4cf42bbd
BURAK UÇAR <burakkucar55@gmail.com>
2026-05-12 21:21:57 +0300

    Service Worker önbellek sorunu giderildi (V2)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dovizim-v1';
+const CACHE_NAME = 'dovizim-v2';
 const ASSETS_TO_CACHE = [
   './index.html',
   './manifest.json',
@@ -10,6 +10,16 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
     })
   );
 });
